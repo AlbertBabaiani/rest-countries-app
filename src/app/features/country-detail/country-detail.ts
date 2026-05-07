c49a4ed84a9016ddf01291122633fb9d3db1d442
Albert Babaiani <babaianialbert@gmail.com>
2026-05-07 13:16:55 +0400

    convert country details to signals

--- src/app/features/country-detail/country-detail.ts
+++ src/app/features/country-detail/country-detail.ts
@@ -1,48 +1,48 @@
-import { AsyncPipe, DecimalPipe } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { catchError, Observable, of, switchMap } from 'rxjs';
+import { DecimalPipe } from '@angular/common';
+import { Component, inject, input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { catchError, filter, Observable, of, switchMap } from 'rxjs';
 import { ICountry } from '../../models/country.model';
 import { MatButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Country } from '../../core/services/country';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-country-detail',
-  imports: [AsyncPipe, DecimalPipe, RouterLink, MatButton, MatIconModule],
+  imports: [DecimalPipe, RouterLink, MatButton, MatIconModule],
   templateUrl: './country-detail.html',
   styleUrl: './country-detail.scss',
 })
-export class CountryDetail implements OnInit {
-  private route = inject(ActivatedRoute);
+export class CountryDetail {
   private router = inject(Router);
   private countryService = inject(Country);
 
-  country$!: Observable<ICountry | null>;
-  borderCountries$!: Observable<ICountry[]>;
+  name = input.required<string>();
 
-  ngOnInit() {
-    this.country$ = this.route.paramMap.pipe(
-      switchMap((params) =>
-        this.countryService.getCountryByName(
-          params.get('name')?.toLocaleLowerCase() || '',
-        ),
-      ),
-      catchError((error) => {
-        console.error('Failed to fetch country:', error);
-        this.router.navigate(['/404']);
-        return of(null);
-      }),
-    );
-    this.borderCountries$ = this.country$.pipe(
-      switchMap((country) => {
-        if (!country || !country.borders || country.borders.length === 0) {
-          return of([]);
-        }
-        return this.countryService.getCountriesByCodes(country.borders);
-      }),
-    );
-  }
+  private country$ = toObservable(this.name).pipe(
+    filter((name) => !!name),
+    switchMap((name) =>
+      this.countryService.getCountryByName(name.toLocaleLowerCase()),
+    ),
+    catchError((error) => {
+      console.error('Failed to fetch country:', error);
+      this.router.navigate(['/404']);
+      return of(null);
+    }),
+  );
+
+  private borderCountries$ = this.country$.pipe(
+    switchMap((country) => {
+      if (!country || !country.borders || country.borders.length === 0) {
+        return of([]);
+      }
+      return this.countryService.getCountriesByCodes(country.borders);
+    }),
+  );
+
+  country = toSignal(this.country$);
+  borderCountries = toSignal(this.borderCountries$, { initialValue: [] });
 
   getFirstNativeName(country: ICountry): string {
     const key = Object.keys(country.name.nativeName)[0];
